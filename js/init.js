a76792b428eb7c56eb1d04cc5f515d77aa7e0ebc
Jason Wilson <nosajnosliw1@JCLW-MacBook-2.local>
2015-05-26 19:31:25 -0400

    sync

--- js/init.js
+++ js/init.js
@@ -7,3 +7,12 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+$('.dropdown-button').dropdown({
+           inDuration: 300,
+           outDuration: 225,
+           constrain_width: false, 
+           hover: true, 
+           gutter: 0, 
+           belowOrigin: true 
+           }
+      );
